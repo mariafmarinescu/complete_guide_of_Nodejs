9b6aaa2f453534e2129402c43aa88f584da7db67
Maria <maria.marinescu@deutschefintechsolutions.de>
2020-08-10 18:43:03 +0200

    deletes duplicate

--- db/SQL/util/database.js
+++
@@ -1,9 +0,0 @@
-const Sequalize = require('sequelize');
-
-
-const sequalize = new Sequalize('nodejs', 'root', 'wacomforever7', { 
-    dialect: 'mysql', 
-    host: 'localhost' 
-});
-
-module.exports = sequalize;
