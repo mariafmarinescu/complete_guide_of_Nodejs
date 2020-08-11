43f6c76278815db8d0e3f52e8dbaba9d3d4cbc4a
Maria <maria.marinescu@deutschefintechsolutions.de>
2020-08-11 17:41:04 +0200

    mongoose fix

--- db/NoSQL/mongoose/controllers/admin.js
+++ db/NoSQL/mongoose/controllers/admin.js
@@ -58,14 +58,14 @@ exports.postEditProduct = (req, res, next) =>{
     const updatedPrice = req.body.price;
     const updatedImageUrl = req.body.imageUrl;
     const updatedDesc = req.body.description;
-    const product = new Product(
-        updatedTitle,
-        updatedPrice,
-        updatedDesc,
-        updatedImageUrl
-    );
-    product
-        .save()
+    Product.findById(prodId)
+        .then(product => {
+            product.title = updatedTitle;
+            product.price = updatedPrice;
+            product.description = updatedDesc;
+            product.imageUrl = updatedImageUrl;
+            return product.save();
+        })
         .then(result =>{
             console.log('product updated')
             res.redirect('/admin/products');
@@ -75,8 +75,7 @@ exports.postEditProduct = (req, res, next) =>{
 
 
 exports.getProducts = (req, res, next) => {
-    req.user
-        .getProducts()
+    Product.find()
         .then(products => {
             res.render('admin/products', {
                 prods: products, 
@@ -90,7 +89,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId;
-    Product.deleteById(prodId)
+    Product.findByIdAndRemove(prodId)
         .then(() => {
             console.log('product deleted successfully');
             res.redirect('/admin/products');
